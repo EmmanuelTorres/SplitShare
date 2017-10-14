d53aca06bd65416b4b54e8255c5280cb49fbb537
emmanuel <emmanueltorres@outlook.com>
2017-10-13 17:37:56 -0700

    Added tests to work on Group and SplitShareUser

--- app/src/main/java/com/splitshare/splitshare/MainActivity.java
+++ app/src/main/java/com/splitshare/splitshare/MainActivity.java
@@ -2,9 +2,8 @@ package com.splitshare.splitshare;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,14 +11,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-
-import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -55,6 +50,8 @@ public class MainActivity extends AppCompatActivity
                 // Adds ourselves
                 group.addMember(splitShareUser.getAccountId());
                 group.addMember("69");
+
+                splitShareUser.getGroups();
 
                 Snackbar.make(view, "Attempting to add default task to database", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
