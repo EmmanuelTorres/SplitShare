2e51e1afc1eb9ee2cf1fb1ebdb6e60b30b119a35
Armando Montanez <montanez.armando.l@gmail.com>
2017-10-16 11:28:54 -0700

    Fix refactor naming for activity stuff

--- app/src/main/java/com/splitshare/splitshare/MainActivity.java
+++ app/src/main/java/com/splitshare/splitshare/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_MainActivity);
+        setContentView(R.layout.activity_mainactivity);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
