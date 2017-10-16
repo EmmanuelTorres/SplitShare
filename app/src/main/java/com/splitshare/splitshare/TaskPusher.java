ccb8ee18671f35416e6573ba7c756ffac957dea5
Armando Montanez <montanez.armando.l@gmail.com>
2017-10-16 10:57:14 -0700

    Renamed groups->MainActivity

--- app/src/main/java/com/splitshare/splitshare/TaskPusher.java
+++ app/src/main/java/com/splitshare/splitshare/TaskPusher.java
@@ -8,7 +8,7 @@ public class TaskPusher {
     /* This class should take a task definition,
      * reformat it, and push it to the database.
      */
-    public static BoolReturnStatus pushTask(MasterTask d, UserAuthentication a) {
+    public static BoolReturnStatus pushTask(MasterTask d /*, UserAuthentication a*/) {
         // push to DB
         return new BoolReturnStatus();
     }
