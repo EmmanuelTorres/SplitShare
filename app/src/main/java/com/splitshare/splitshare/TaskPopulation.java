d1ccd92c54ce743bd77a4908cd779a36c37cb22e
Armando Montanez <montanez.armando.l@gmail.com>
2017-11-27 10:39:28 -0800

    Prepare for appending tasks to list

--- app/src/main/java/com/splitshare/splitshare/TaskPopulation.java
+++ app/src/main/java/com/splitshare/splitshare/TaskPopulation.java
@@ -10,11 +10,12 @@ import java.util.Calendar;
 
 public class TaskPopulation
 {
+    public static Calendar today;
     public static void populate()
     {
         SplitShareApp.populatedTask.clear();
         for(MasterTask a : SplitShareApp.usersMasterTasks){
-            Calendar today = Calendar.getInstance();
+            today = Calendar.getInstance();
 
             today.set(Calendar.HOUR_OF_DAY, 0);
             today.set(Calendar.MINUTE, 0);
@@ -22,25 +23,21 @@ public class TaskPopulation
             today.set(Calendar.MILLISECOND, 0);
 
             //if last date of task < today, then skip this task
-            if(a.getEndDate().before(today)){
+            if(a.getEndDate().before(today) && !a.isForever()){
                 continue;
             } else {
-
-                int limit = 0;
                 Calendar currentDay = (Calendar)today.clone();
                 //breaks after either one month or 5 tasks spawn from this master task
-                for(int i = 0; i< 100; i++){
-                    if(limit == 20){break;}
+                for (int i = 0; i < 7; i++) {
                    //if there is an event for this day
-                   if(a.getCycle().isOnDayWithStart(a.getStartDate(), currentDay)){ //TODO: this will work when we can have a list of members in a MasterTask
+                   if (a.getCycle().isOnDayWithStart(a.getStartDate(), currentDay)) {
                        int whosTurn = a.getCycle().numOcurrencesSinceStart(a.getStartDate(), currentDay) % a.getActiveUsers().size();
                        String user1 = a.getActiveUsers().get(whosTurn);
                        // only add to agenda if this task is mine
                        if (user1.compareTo(SplitShareApp.splitShareUser.getUserId()) == 0) {
 
-                           if (a.getEndDate().before(currentDay))
+                           if (a.getEndDate().before(currentDay) && !a.isForever())
                                break;
-                           limit++;
                            //Create dumb task
                            SplitShareApp.populatedTask.add(a.createDumbTask((Calendar) currentDay.clone()));
                        }
@@ -52,7 +49,8 @@ public class TaskPopulation
             }
 
         }
-
+        // next refresh is 7 days later
+        today.add(Calendar.DATE, 7);
         MainActivity.addAllToTaskList(SplitShareApp.populatedTask);
         MainActivity.swipeToRefresh.setRefreshing(false);
     }
